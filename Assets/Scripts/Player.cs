7b131b326bb8c92d3def941903553a98b923aa19
Azeem Bande-Ali <a.bandeali@gmail.com>
2018-07-19 22:52:46 -0400

    Better jumping, stop rotating flipper, clamp horizontal velocity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,17 @@ public class Player : MonoBehaviour {
     public int jumpTimesAllowed = 2;
     public int speed = 10;
     public int jumpForce = 10;
+    public int maxVelX = 10;
 
     Rigidbody2D rigid;
+    GameObject flipper;
     bool onGround = false;
     int jumpCount = 0;
 
 	// Use this for initialization
 	void Start () {
         rigid = GetComponent<Rigidbody2D>();
+        flipper = transform.Find("Flipper").gameObject;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +26,7 @@ public class Player : MonoBehaviour {
             if (onGround || jumpCount < jumpTimesAllowed)
             {
                 jumpCount++;
+                rigid.velocity = new Vector2(rigid.velocity.x, 0f);
                 rigid.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
             }
         }
@@ -30,6 +34,26 @@ public class Player : MonoBehaviour {
         float inAirMultiplier = onGround ? 1f : 0.5f;
         float horizontal = Input.GetAxis("Horizontal") * speed * inAirMultiplier;
         rigid.AddForce(transform.right * horizontal);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("stop motor");
+            HingeJoint2D joint = flipper.GetComponent<HingeJoint2D>();
+            joint.useMotor = false;
+            flipper.GetComponent<Rigidbody2D>().angularVelocity = 0f;
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            Debug.Log("restart motor");
+            HingeJoint2D joint = flipper.GetComponent<HingeJoint2D>();
+            joint.useMotor = true;
+        }
+        
+        if (Mathf.Abs(rigid.velocity.x) > maxVelX)
+        {
+            Debug.Log("Trimming horizontal velocity");
+            rigid.velocity = new Vector2(maxVelX * Mathf.Sign(rigid.velocity.x), rigid.velocity.y);
+        }
 	}
 
     private void OnCollisionEnter2D(Collision2D collision)

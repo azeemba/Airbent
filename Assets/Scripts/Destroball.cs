dacc7c1e4dda09cf1a4fc27263e75bde6d51dfb9
Azeem Bande-Ali <a.bandeali@gmail.com>
2018-08-01 14:20:20 -0400

    Destroball and destrowall!

---
+++ Assets/Scripts/Destroball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground") ||
+            collision.collider.CompareTag("Wall"))
+        {
+            Debug.Log("Ball hit ground or wall");
+            Destroy(gameObject);
+        }
+    }
+}
